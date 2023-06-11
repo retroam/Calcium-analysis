a6b26ffc716ed967ca59f6fc30efbe951302a869
retroam <retroam@yahoo.com>
2023-06-11 20:25:12 +0000

    adding some empty files

---
+++ tests/test_image_processing.py
@@ -0,0 +1,20 @@
+import unittest
+import numpy as np
+import os
+from PIL import Image
+import image_processing  # assuming the module is named image_processing.py
+
+class TestImageProcessing(unittest.TestCase):
+    
+    def test_load_image(self):
+        # Test loading a single image
+        image_path = 'path_to_a_test_image.tif'
+        I, info = image_processing.load_image(image_path)
+
+        self.assertIsInstance(I, np.ndarray)
+        self.assertIsInstance(info, dict)
+        self.assertEqual(I.shape[0], info['ImageLength'])
+        self.assertEqual(I.shape[1], info['ImageWidth'])
+
+if __name__ == '__main__':
+    unittest.main()
