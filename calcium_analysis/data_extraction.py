6861bcd9b1ddfabdc487b4c4fa45ffc6bf0f72d7
retroam <retroam@yahoo.com>
2023-06-13 03:49:56 +0000

    adding additional code;not tested

---
+++ calcium_analysis/data_extraction.py
@@ -0,0 +1,40 @@
+import numpy as np
+from skimage import measure
+import os
+
+def roi_to_data(roi_file, image_file):
+    """
+    Extract data from regions of interest in an image stack.
+
+    This function loads a labeled image (from a .npy file), computes
+    the mean intensity of each labeled region in each image in a stack,
+    and saves the results.
+    """
+
+    # Load the labeled image and the image stack
+    labels = np.load(roi_file)
+    I_t = np.load(image_file)
+
+    # Extract data from each region in each image
+    data = []
+    bkg = []
+    for I in np.rollaxis(I_t, axis=2):
+        # Get properties of labeled regions
+        props = measure.regionprops(labels, intensity_image=I)
+        
+        # Compute the mean intensity of each region and the background
+        avg = [prop.mean_intensity for prop in props]
+        I_bkg = I.copy()
+        for prop in props:
+            I_bkg[prop.coords[:, 0], prop.coords[:, 1]] = np.nan
+        bkg_intensity = np.nanmean(I_bkg)
+
+        data.append(avg)
+        bkg.append(bkg_intensity)
+
+    # Save the results
+    np.save('data.npy', data)
+    np.save('bkg.npy', bkg)
+
+    # Return the results
+    return data, bkg
--- calcium_analysis/image_processing.py
+++ calcium_analysis/image_processing.py
@@ -1,12 +1,44 @@
-import os
+import imageio.v3 as iio
+from skimage import filters, measure, segmentation, feature, io
+from scipy import ndimage as ndi
+import matplotlib.pyplot as plt
+from PIL import Image
+from PIL.TiffTags import TAGS
 import numpy as np
-from PIL import Image 
 
 def load_image(image_path):
     """
     Load an image and return its data and metadata
     """
+    image = iio.imread(uri=image_path)
     with Image.open(image_path) as img:
-        info = img._getexif()
-        image_np = np.asarray(img)
-    return image_np, info
+        info = {TAGS[key] : img.tag[key] for key in img.tag.keys()}
+
+    return image, info
+
+def image_to_stack(image_fldr, save_fldr):
+    """Assembles tif images in image_fld into a single numpy array."""
+    image_files = [f for f in os.listdir(image_fldr) if f.endswith('.tif')]
+    image_count = len(image_files)
+    print(f"Processing {image_count} images...")
+    I_t = []
+
+    for j, image_file in enumerate(image_files, 1):
+        image_path = os.path.join(image_fld, image_file)
+        print(f"Loading image {j} of {image_count} from {image_path}")
+        
+        I, info = load_image(image_path)
+        num_images = len(info)
+        image_shape = (info[0]['ImageLength'], info[0]['ImageWidth'], num_images)
+        print(f"Image has shape {image_shape}")
+
+        I_cum = np.zeros(image_shape)
+        for k in range(num_images):
+            I_cum[:, :, k] = I
+        I_t.append(I_cum)
+
+    I_t = np.concatenate(I_t, axis=2)
+
+    output_path = os.path.join(save_fld, 'I.npy')
+    print(f"Saving output to {output_path}")
+    np.save(output_path, I_t)
