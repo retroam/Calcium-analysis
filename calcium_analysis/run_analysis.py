ce655eeb2025e566badec45b821caf0cda4ffed3
Robert Amanfu <robertamanfu@Roberts-MacBook-Pro.local>
2023-06-15 21:01:13 -0700

    add run file

---
+++ calcium_analysis/run_analysis.py
@@ -0,0 +1,9 @@
+import os
+from .image_processing import image_to_stack
+from .data_extraction import roi_to_data
+from .image_segmentation import stack_to_roi
+
+def run_analysis(image_fld, save_fld, sd):
+    image_to_stack(image_fld, save_fld)
+    stack_to_roi(save_fld, sd)
+    roi_to_data(save_fld)
