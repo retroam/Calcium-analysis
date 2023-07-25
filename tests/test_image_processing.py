055c167be0b4a5168647bdff5103001f18eb9995
retroam <retroam@yahoo.com>
2023-07-25 06:17:46 +0000

    adding init file

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -2,14 +2,14 @@ import unittest
 import numpy as np
 import os
 from PIL import Image
-import calcium_analysis.image_processing  # assuming the module is named image_processing.py
+from calcium_analysis.image_processing import load_image
 
 class TestImageProcessing(unittest.TestCase):
     
     def test_load_image(self):
         # Test loading a single image
         image_path = 'path_to_a_test_image.tif'
-        I, info = image_processing.load_image(image_path)
+        I, info = load_image(image_path)
 
         self.assertIsInstance(I, np.ndarray)
         self.assertIsInstance(info, dict)
